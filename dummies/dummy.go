352f12adeefbc847af618382f9320c4c3fff2898
turanukimaru <turanukimaru@gmail.com>
2021-12-27 19:10:16 +0900

    サブディレクトリに go.mod があるとサブモジュールになるので削除。 上位の go.mod は配下のディレクトリをソースとして使うときに go.mod があるディレクトリは除外するようだ。  ...じゃあサブモジュールの意味はなんだ？

--- dummies/dummy.go
+++ dummies/dummy.go
@@ -2,7 +2,7 @@ package dummies
 
 import (
 	"fmt"
-	"github.com/turanukimaru/goastart/dummydb"
+	"github.com/turanukimaru/gormstart/pkg/dummydb"
 )
 
 type Dummy struct {
@@ -10,6 +10,14 @@ type Dummy struct {
 
 func (d *Dummy) Hello() (err error) {
 	fmt.Println("Hello Dummies !")
+	// お試しという事でDBアクセスだけ別モジュールにしてみる。
+	// ローカルを参照するには
+	//	go mod edit -replace github.com/turanukimaru/gormstart=../gormstart
+	// なお、バージョンの都合で次のエラーが発生することがある。ので go get -u でバージョンを合わせる。
+	//go: finding module for package github.com/turanukimaru/goastart/dummydb
+	//github.com/turanukimaru/goastart/calcapi imports
+	//	github.com/turanukimaru/goastart/dummydb: no matching versions for query "latest"
+	// go get -u github.com/turanukimaru/gormstart
 	err = dummydb.DbAccess()
 	return
 }
